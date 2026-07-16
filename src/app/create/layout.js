acf3285aba9994591d651296c0da5be07c677afc
RaeChoe <picasomati@gmail.com>
2026-07-16 15:39:57 +0900

    feat: enhance Create page layout with styles and add main.js script

--- src/app/create/layout.js
+++ src/app/create/layout.js
@@ -1,10 +1,12 @@
+import styles from "./create.module.css";
+
 // Create 페이지의 틀
 export default function Layout({ children }) {
-  console.log("Create Page 작동");
+  console.log("Create 레이아웃 작동");
 
   return (
     <form>
-      <h2>Create Page</h2>
+      <h2 className={styles.title}>Create Page</h2>
       {/* page.js 불러오기: Create 페이지의 알맹이 */}
       {children}
     </form>
